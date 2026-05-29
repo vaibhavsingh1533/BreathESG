ac94331e33514e708a50ea558b4b5a863ab478fb
Vaibhav singh <vaibhavpalsingh1234@gmail.com>
2026-05-29 15:34:41 +0530

    Prepare deployment

--- backend/core/settings.py
+++ backend/core/settings.py
@@ -82,14 +82,12 @@ WSGI_APPLICATION = 'core.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/6.0/ref/settings/#databases
 
+from pathlib import Path
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'breatheesg',
-        'USER': 'root',
-        'PASSWORD': '8827',
-        'HOST': 'localhost',
-        'PORT': '3306',
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
 

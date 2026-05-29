7546eedd5c8adf8d7855915f463ea2a781afe1a6
Vaibhav singh <vaibhavpalsingh1234@gmail.com>
2026-05-29 15:42:01 +0530

    Configure Render deployment

--- backend/core/settings.py
+++ backend/core/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-p4=bfbg9tc1bq!u*+*51jp+3qdrjeg4ii^@yu)35^*zg8!th#o'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [*]
 
 
 # Application definition
